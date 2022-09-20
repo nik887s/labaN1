fbbf4e9194f5e0bbe3da574dd839858885bfbf8b
nik887s <113353404+nik887s@users.noreply.github.com>
2022-09-20 23:03:46 +0300

    Добавьте файлы проекта.

---
+++ laba1.1/laba1.1.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main() {
+    float rebro, volume, sidearea;
+    printf("input the rebro of the cube: ");
+    if (scanf_s("%f", &rebro) == 1 && rebro >= 0)
+    {
+        volume = rebro * rebro * rebro;
+         sidearea = 4 * rebro * rebro;
+        printf("Volume of a cube= %.3f\nSide surface area of a cube= %.3f\n", volume, sidearea);
+
+    }
+    else
+    {
+        printf("Uncorrect input");
+    }
+    return 0;
+}
